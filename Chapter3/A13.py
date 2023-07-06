69ead8a5e6c7ad94a5e0923fe4644e44415fd1ff
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2023-07-06 09:30:39 +0900

    Create A13.py

---
+++ Chapter3/A13.py
@@ -0,0 +1,22 @@
+# 入力
+N,K = map(int,input().split())
+A = list(map(int,input().split()))
+
+R = [ None ] * N
+
+# しゃくとり法
+for i in range(N-1):
+  # スタート地点
+  if i==0:
+    R[i] = 0
+  else:
+    R[i] = R[i - 1]
+  # 限界まで増やす
+  while R[i] < N-1 and A[R[i]+1]-A[i] <= K:
+    R[i] += 1
+
+# 出力
+Answer = 0
+for i in range(N-1):
+  Answer += (R[i] - i)
+print(Answer)
