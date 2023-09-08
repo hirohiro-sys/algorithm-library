49ba4a6dbe4f17f27c7414a575661487e2d1ea0d
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2023-09-08 14:27:50 +0900

    Update B61.py

--- Chapter9/B61.py
+++ Chapter9/B61.py
@@ -5,11 +5,11 @@ N, M = map(int, input().split())
 friend = [0] * N
 for _ in range(M):
     A, B = map(int, input().split())
-    A -= 1
-    B -= 1
-    friend[A] += 1
-    friend[B] += 1
+# インデックスは0から始まるから−１する
+    friend[A-1] += 1
+    friend[B-1] += 1
 
 mx = max(friend)
 ans = friend.index(mx)
+# インデックスもどす
 print(ans + 1)
