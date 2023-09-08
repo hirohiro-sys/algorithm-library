4dd77c77d09d1ef147357b87b0b624948d7bc48a
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2023-09-08 22:28:25 +0900

    Update A63.py

--- Chapter9/A63.py
+++ Chapter9/A63.py
@@ -1,29 +1,38 @@
 from collections import deque
 
 # 入力
-N, M = map(int, input().split())
-edges = [ list(map(int, input().split())) for i in range(M) ]
+n,m = map(int,input().split())
 
-# 隣接リストの作成
-G = [ list() for i in range(N + 1) ]
-for a, b in edges:
-	G[a].append(b)
-	G[b].append(a)
+edges = [list(map(int,input().split())) for _ in range(m)]
 
-# 幅優先探索の初期化（dist[i] = ? ではなく dist[i] = -1 で初期化していることに注意）
-dist = [ -1 ] * (N + 1)
+# 隣接リストの初期化と構築
+G = [[] for i in range(n+1)]
+for a,b in edges:
+    G[a].append(b)
+    G[b].append(a)
+
+# 各頂点までの距離を保持するリストdistを初期化
+dist = [-1] * (n + 1)
+
+# 始点からの距離を初期化
 dist[1] = 0
+
+# 幅優先探索のキューを初期化
 Q = deque()
 Q.append(1)
 
-# 幅優先探索
+# 幅優先探索メイン処理
 while len(Q) >= 1:
-	pos = Q.popleft() # キュー Q の先頭要素を取り除き、その値を pos に代入する
-	for nex in G[pos]:
-		if dist[nex] == -1:
-			dist[nex] = dist[pos] + 1
-			Q.append(nex)
+    pos = Q.popleft()
+    for nex in G[pos]:
+        if dist[nex] == -1:
+            dist[nex] = dist[pos] + 1
+            Q.append(nex)
 
-# 頂点 1 から各頂点までの最短距離を出力
-for i in range(1, N + 1):
-	print(dist[i])
+for i in range(1,n + 1):
+    print(dist[i])
+
+
+'''
+書籍の図を見ながらコードを見ていくとわかりやすい
+'''
