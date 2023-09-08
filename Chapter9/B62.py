ff497c432778efbb4d67ab4e8f059ef67ab49301
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2023-09-08 15:22:59 +0900

    Update B62.py

--- Chapter9/B62.py
+++ Chapter9/B62.py
@@ -1,34 +1,37 @@
 import sys
-sys.setrecursionlimit(1 << 20)  # 再帰上限がデフォルトで 1000 なので変更する
+
+sys.setrecursionlimit(1 << 20)
 
 # 入力
-N, M = map(int, input().split())
-g = [[] for _ in range(N)]
-for i in range(M):
-    A, B = map(int, input().split())
-    A -= 1
-    B -= 1
-    g[A].append(B)
-    g[B].append(A)
+n, m = map(int,input().split())
 
-# 深さ優先探索
-visited = [False] * (N + 1)
+# 隣接リスト初期化&構築
+g  = [[] for _ in range(n)]
+for i in range(m):
+    a,b = map(int,input().split())
+    g[a-1].append(b)
+    g[b-1].append(a)
+
+# ノードの訪問状態管理配列を初期化
+visited = [False] * n
+
+# パスを記録するリスト
 path = []
 
+# 深さ優先探索メイン実装
 def dfs(i: int):
     path.append(i)
-    # ゴール地点にたどり着いた！
-    if i == N - 1:
-        # 答えを出力して終了
+
+    if i==n - 1:
         for x in path:
-            print(x + 1) # 1-indexed に変更
+            print(x + 1)
         exit(0)
 
-    # その他の場合
     visited[i] = True
     for j in g[i]:
         if not visited[j]:
             dfs(j)
     path.pop()
 
+# 深さ優先探索
 dfs(0)
