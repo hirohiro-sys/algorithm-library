bd8ddb5f6638344367108da35ee71c43850a7f31
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-04 10:07:26 +0900

    基本的な全探索

--- Chapter 1/B03.py
+++ Chapter 1/B03.py
@@ -1,29 +1,13 @@
 n = int(input())
-li = list(map(int,input().split()))
+a = list(map(int,input().split()))
 
 for i in range(n):
-  for j in range(i+1, n):
-    for k in range(j+1, n):
-      if li[i]+li[j]+li[k]==1000:
+  for j in range(i+1,n):
+    for k in range(j+1,n):
+      if a[i]+a[j]+a[k]==1000:
         exit(print("Yes"))
 print("No")
 
-'''
-# 入力
-N = int(input())
-A = list(map(int, input().split()))
-
-# 答えを求める
-Answer = False
-for i in range(N):
-	for j in range(i+1, N):
-		for k in range(j+1, N):
-			if A[i] + A[j] + A[k] == 1000:
-				Answer = True
-
-# 出力
-if Answer == True:
-	print("Yes")
-else:
-	print("No")
-'''
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cb
+"""
