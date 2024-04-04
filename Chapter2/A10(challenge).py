aac6f144d2a4c700ff0fb82f473a1a442f27b9c1
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-04 09:21:53 +0900

    累積和チャレンジ問題

--- Chapter2/A10(challenge).py
+++ Chapter2/A10(challenge).py
@@ -2,23 +2,27 @@
 N = int(input())
 A = list(map(int,input().split()))
 D = int(input())
-L = [ None ] * D
-R = [ None ] * D
-for i in range(D):
-    L[i],R[i] = map(int,input().split())
+query = [list(map(int,input().split())) for _ in range(D)]
+for q in query:
+  q[0] -= 1
+  q[1] -= 1
 
-# P[i]
-P = [ None ] * N
-P[0] = A[0]
+# 左からの最大累積和
+left_max = [0] * N
+left_max[0] = A[0]
 for i in range(1,N):
-    P[i] = max(P[i-1],A[i])
-
-# Q[i]
-Q = [ None ] * N
-Q[N-1] = A[N-1]
-for i in reversed(range(0,N-1)):
-    Q[i] = max(Q[i+1], A[i])
+  left_max[i] = max(left_max[i-1],A[i])
+  
+# 右からの最大累積和
+right_max = [0] * N
+right_max[-1] = A[-1]
+for i in range(N-2,-1,-1):
+  right_max[i] = max(right_max[i+1],A[i])
 
 # 出力
-for i in range(D):
-    print(max(P[(L[i]-1)-1], Q[(R[i]+1)-1]))
+for l,r in query:
+  print(max(left_max[l-1],right_max[r+1]))
+
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_j
+"""
