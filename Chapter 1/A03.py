aa1696a40dc3c8a7a9c6db1bca30c629c925f2c6
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-04 10:05:18 +0900

    基本的な全探索

--- Chapter 1/A03.py
+++ Chapter 1/A03.py
@@ -1,29 +1,13 @@
 n,k = map(int,input().split())
-P = list(map(int,input().split()))
-Q = list(map(int,input().split()))
- 
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
 for i in range(n):
   for j in range(n):
-    if P[i]+Q[j]==k:
+    if a[i]+b[j]==k:
       exit(print("Yes"))
 print("No")
 
-'''
-# 入力
-N, K = map(int, input().split())
-P = list(map(int, input().split()))
-Q = list(map(int, input().split()))
-Answer = False
-
-# 全探索（Answer は「合計が K になる選び方が見つかったか」を示す）
-for x in range(N):
-	for y in range(N):
-		if P[x] + Q[y] == K:
-			Answer = True
-
-# 出力
-if Answer == True:
-	print("Yes")
-else:
-	print("No")
-'''
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_c
+"""
