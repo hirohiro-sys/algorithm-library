4f469081c41f6aa8268e40035826c546e20c08e2
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-04 09:45:05 +0900

    基本的な2次元のいもす法

--- Chapter2/A09.py
+++ Chapter2/A09.py
@@ -1,34 +1,25 @@
-# 入力
 H,W,N = map(int,input().split())
-A = [ None ] * N
-B = [ None ] * N
-C = [ None ] * N
-D = [ None ] * N
-X = [ None ] * W
+imos_array = [[0]*(W+2) for _ in range(H+2)]
+presum_array = [[0]*(W+2) for _ in range(H+2)]
+# 書籍参考に二次元の前日比とる
 for i in range(N):
-    A[i],B[i],C[i],D[i] = map(int,input().split())
+  a,b,c,d = map(int,input().split())
+  imos_array[a][b] += 1
+  imos_array[c+1][d+1] += 1
+  imos_array[a][d+1] -= 1
+  imos_array[c+1][b] -= 1
+# 横の累積和
+for i in range(H):
+  for j in range(W):
+    presum_array[i+1][j+1] = presum_array[i+1][j] + imos_array[i+1][j+1]
+# 縦の累積和
+for j in range(W):
+  for i in range(H):
+    presum_array[i+1][j+1] = presum_array[i+1][j+1] + presum_array[i][j+1]
 
-# 格日の前日比(解説みる)
-X = [ [ 0 ] * (W + 2) for _ in range(H + 2) ]
-Z = [ [ 0 ] * (W + 2) for _ in range(H + 2) ]
-for i in range(N):
-    X[A[i]][B[i]] += 1
-    X[A[i]][D[i]+1] -= 1
-    X[C[i]+1][B[i]] -= 1
-    X[C[i]+1][D[i]+1] += 1
+for i in range(H):
+  print(*presum_array[i+1][1:W+1])
 
-# 累積和
-for i in range(1,H+1):
-    for j in range(1,W+1):
-        Z[i][j] = Z[i][j-1] + X[i][j]
-for j in range(1,W+1):
-    for i in range(1,H+1):
-        Z[i][j] = Z[i-1][j] + Z[i][j]
-
-# 出力
-for i in range(1, H+1):
-    for j in range(1, W+1):
-        if j>=2:
-            print(" ", end="")
-        print(Z[i][j],end="")
-    print("")
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_i
+"""
