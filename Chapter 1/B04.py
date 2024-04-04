69326c80d26144c45f7ca78286c766311411b8c0
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-04 10:14:59 +0900

    2進数を10進数に変換

--- Chapter 1/B04.py
+++ Chapter 1/B04.py
@@ -1,19 +1,5 @@
-n = input()
-print(int(n,2))
+print(int(int(input()),2))
 
 '''
-# 入力
-N = input()
-
-# 答えを求める
-Answer = 0
-for i in range(len(N)):
-	keta = 0
-	kurai = (2 ** (len(N) - 1 - i))
-	if N[i] == '1':
-		keta = 1
-	Answer += keta * kurai
-
-# 出力
-print(Answer)
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cc
 '''
