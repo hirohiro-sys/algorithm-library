d03a1e99e3a5f382e54ac071f5add7cd32632f53
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-04 09:25:22 +0900

    基本的な1次元の累積和

--- Chapter2/A06.py
+++ Chapter2/A06.py
@@ -1,28 +1,12 @@
-#そのまま実装
 n,q = map(int,input().split())
-A = [0]
-A.extend(list(map(int,input().split())))
-li = [0]
+a = list(map(int,input().split()))
+s = [0] * (n+1)
+s[0] = 0
 for i in range(1,n+1):
-  # li[i-1]を足さないと累積和が作れない
-  li.append(A[i]+li[i-1])
-for _ in range(q):
-  l,r = map(int,input().split())
-  print(li[r]-li[l-1])
-
-'''
-N, Q = map(int, input().split())
-A = list(map(int, input().split()))
-L = [ None ] * Q
-R = [ None ] * Q
-for j in range(Q):
-	L[j], R[j] = map(int, input().split())
- 
-S = [ None ] * (N + 1)
-S[0] = 0
-for i in range(N):
-	S[i + 1] = S[i] + A[i]
- 
-for j in range(Q):
-	print(S[R[j]] - S[L[j] - 1])
-'''
+  s[i] = s[i-1]+a[i-1]
+for i in range(q):
+  a,b = map(int,input().split())
+  print(s[b]-s[a-1])
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_ai
+"""
