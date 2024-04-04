8e709d1b5caf0798f26ecf51b341bef107fe0080
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-04 09:32:13 +0900

    応用的な1次元のいもす法

--- Chapter2/B07.py
+++ Chapter2/B07.py
@@ -1,27 +1,15 @@
-# 入力
 T = int(input())
 N = int(input())
-L = [ None ] * N
-R = [ None ] * N
+A = [0] * (T+1)
 for i in range(N):
-    L[i],R[i] = map(int,input().split())
-
-#前の時刻との差
-B = [ 0 ] * (T + 1)
-for i in range(N):
-    B[L[i]] += 1
-    B[R[i]] -= 1
-
-#累積和
-Answer = [ None ] * (T + 1)
-Answer[0] = B[0]
-for i in range(1,T+1):
-    Answer[i] = Answer[i-1] + B[i]
-
-#出力
+  L,R = map(int,input().split())
+  A[L] += 1
+  A[R] -= 1
+ans = 0
 for i in range(T):
-    print(Answer[i])
+  ans += A[i]
+  print(ans)
 
-'''
-これもいもす法
-'''
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_al
+"""
