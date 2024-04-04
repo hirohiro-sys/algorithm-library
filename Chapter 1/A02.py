81f981559140d6e8ed106bc37b73e7e77ac3b343
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-04 10:02:41 +0900

    基本的な全探索

--- Chapter 1/A02.py
+++ Chapter 1/A02.py
@@ -5,20 +5,6 @@ for i in range(n):
     exit(print("Yes"))
 print("No")
 
-'''
-# 入力
-N, X = map(int, input().split())
-A = list(map(int, input().split()))
-Answer = False
-
-# 全探索（変数 Answer は「既に x が見つかったかどうか」を表す）
-for i in range(N):
-	if A[i] == X:
-		Answer = True
-
-# 出力
-if Answer == True:
-	print("Yes")
-else:
-	print("No")
-'''
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_b
+"""
