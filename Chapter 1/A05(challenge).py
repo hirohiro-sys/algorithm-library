a89a8570d1c4d13b0484a695bf4359b5710c5e87
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-04 10:09:29 +0900

    計算量意識した全探索

--- Chapter 1/A05(challenge).py
+++ Chapter 1/A05(challenge).py
@@ -1,24 +1,11 @@
 n,k = map(int,input().split())
-count = 0
+ans = 0
 for i in range(1,n+1):
   for j in range(1,n+1):
-    tmp = k - i - j
-    if 1<=tmp<=n:
-      count+=1
-print(count)
+    if 1<=k-i-j<=n:
+      ans += 1
+print(ans)
 
-'''
-# 入力
-N, K = map(int, input().split())
-Answer = 0
-
-# 全探索
-for x in range(1, N+1):
-	for y in range(1, N+1):
-		z = K - x - y
-		if z >= 1 and z <= N:
-			Answer += 1
-
-# 出力
-print(Answer)
-'''
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_e
+"""
