417a3051c13d6bb9c1756d3320b456a4f3292a22
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-06 15:24:50 +0900

    基本的なグラフ実装①

--- Chapter9/A61.py
+++ Chapter9/A61.py
@@ -1,18 +1,14 @@
-# 入力
-N, M = map(int, input().split())
-edges = [ list(map(int, input().split())) for i in range(M) ]
+N,M = map(int,input().split())
+G = [[] for _ in range(N)]
 
-# 隣接リストの作成
-G = [ list() for i in range(N + 1) ] # G[i] は頂点 i に隣接する頂点のリスト
-for a, b in edges:
-	G[a].append(b) # 頂点 a に隣接する頂点として b を追加
-	G[b].append(a) # 頂点 b に隣接する頂点として a を追加
+for i in range(M):
+  a,b = map(int,input().split())
+  G[a-1].append(b)
+  G[b-1].append(a)
 
-# 出力
-for i in range(1, N + 1):
-	output = ''
-	output += str(i)
-	output += ': {'
-	output += ', '.join(map(str, G[i])) # 例えば G[i] = { 2, 7, 5 } のとき、ここで "2, 7, 5" という文字列が output の後ろに連結される
-	output += '}'
-	print(output)
+for i in range(N):
+  print(str(i+1)+':',set(G[i]) if len(G[i]) != 0 else '{}') 
+
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bi
+"""
