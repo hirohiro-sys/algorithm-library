b9a8673c28c816590f556fadab08c40143bc16a6
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-13 20:32:10 +0900

    基本的な二分探索

--- Chapter3/A11.py
+++ Chapter3/A11.py
@@ -1,22 +1,24 @@
-# 整数 x が何番目に存在するかを返す関数
-def search(x, A):
-	L = 0
-	R = N-1
-	while L <= R:
-		M = (L + R) // 2
-		if x < A[M]:
-			R = M - 1
-		if x == A[M]:
-			return M
-		if x > A[M]:
-			L = M + 1
-	return -1 
+n,x = map(int,input().split())
+a = list(map(int,input().split()))
 
+l = 0
+r = n-1
+while l<=r:
+  m = (l+r)//2
+  if x<a[m]:
+    r = m-1
+  elif x>a[m]:
+    l = m+1
+  else:
+    exit(print(m+1))
 
-# 入力
-N, X = map(int, input().split())
-A = list(map(int, input().split()))
+"""bisect使った簡潔な書き方
+import bisect
+n,x = map(int,input().split())
+a = list(map(int,input().split()))
+print(bisect.bisect(a,x))
+"""
 
-# 出力
-Answer = search(X, A)
-print(Answer + 1)
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_k
+"""
