e19d2bbb78f9449022342959c61eb5bafecfe7cf
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-13 20:32:59 +0900

    基本的な二分探索②

--- Chapter3/B11.py
+++ Chapter3/B11.py
@@ -1,18 +1,7 @@
-# 二分探索ライブラリ
 import bisect
+n,a = int(input()),sorted(map(int,input().split()))
+for i in range(int(input())): print(bisect.bisect_left(a,int(input())))
 
-# 入力
-N = int(input())
-A = list(map(int,input().split()))
-Q = int(input())
-X = [ None ] * Q
-for i in range(Q):
-    X[i] = int(input())
-
-# 配列は昇順でないといけないためsort
-A.sort()
-
-for i in range(Q):
-    # bisect_leftはX[i]がAの左から見て何番目に入るか
-    ans = bisect.bisect_left(A,X[i])
-    print(ans)
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cj
+"""
