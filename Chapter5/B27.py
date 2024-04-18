ce78d1f7e947cd9b7c93f188be153a614c59e75c
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-18 12:45:12 +0900

    最小公倍数を求める

--- Chapter5/B27.py
+++ Chapter5/B27.py
@@ -1,17 +1,7 @@
-def GCD(A, B):
-	while A >= 1 and B >= 1:
-		if A >= B:
-			A = A % B # A の値を変更する場合
-		else:
-			B = B % A # B の値を変更する場合
-	if A >= 1:
-		return A
-	return B
-
-# 入力
-A, B = map(int, input().split())
-print(A * B // GCD(A, B))
+import math
+a,b = map(int,input().split())
+print(a*b//math.gcd(a,b))
 
 """
-二つの値の積を最大公約数で割ると最小公倍数が出る
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cz
 """
