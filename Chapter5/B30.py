a5adaaf5fdd9138b45bf17346819dd61fb894bc6
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-21 10:03:10 +0900

    組み合わせを求める(2)

--- Chapter5/B30.py
+++ Chapter5/B30.py
@@ -1,39 +1,13 @@
-# a の b 乗を m で割った余りを返す関数
-def Power(a, b, m):
-	p = a
-	Answer = 1
-	for i in range(30):
-		wari = 2 ** i
-		if (b // wari) % 2 == 1:
-			Answer = (Answer * p) % m # a の 2^i 乗が掛けられるとき
-		p = (p * p) % m
-	return Answer
+import math
+H,W = map(int, input().split())
+print(math.comb(H+W-2,W-1)%1000000007)
 
-# a÷b を m で割った余りを返す関数
-def Division(a, b, m):
-	return (a * Power(b, m - 2, m)) % m
+"""公式解説
+マス(1,1)から(H,W)まで行くためにはH+W-2であり、その中のW-1が右方向である必要がある。
+逆に右方向への移動回数がW-1なら必ず(H,W)でゴールできる。したがって求める移動方法の数は
+H+W-2個の中からW-1個を選ぶ方法の数通りになる。
+"""
 
-# nCr mod 1000000007 を返す関数
-def ncr(n, r):
-	M = 1000000007;
-
-	# 手順 1: 分子 a を求める
-	a = 1;
-	for i in range(1, n+1):
-		a = (a*i) % M
-
-	# 手順 2: 分母 b を求める
-	b = 1;
-	for i in range(1, r+1):
-		b = (b * i) % M
-	for i in range(1, n-r+1):
-		b = (b * i) % M
-
-	# 手順 3: 答えを求める
-	return Division(a, b, M);
-
-# 入力
-H, W = map(int, input().split())
-
-# 出力
-print(ncr(H + W - 2, W - 1))
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_dc
+"""
