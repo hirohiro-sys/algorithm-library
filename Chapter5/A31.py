c881d0ff071bee2b5dc879513e9a6d6933b86832
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-22 17:06:53 +0900

    包除原理

--- Chapter5/A31.py
+++ Chapter5/A31.py
@@ -1,5 +1,6 @@
-N = int(input())
-A1 = (N //  3) # 3 で割り切れるものの個数
-A2 = (N //  5) # 5 で割り切れるものの個数
-A3 = (N // 15) # 3, 5 両方で割り切れるもの（= 15 の倍数）の個数
-print(A1 + A2 - A3)
+n = int(input())
+print(n//3+n//5-n//15)
+
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ae
+"""
