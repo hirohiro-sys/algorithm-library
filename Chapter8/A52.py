cfef63d159b51da2779dae5f5bc7615010397d5b
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-24 20:52:21 +0900

    基本的なキュー

--- Chapter8/A52.py
+++ Chapter8/A52.py
@@ -1,15 +1,14 @@
-from collections import deque
+queue = []
+for i in range(int(input())):
+  a = input().split()
+  if a[0]=="1":
+    queue.append(a[1])
+  elif a[0]=="2":
+    print(queue[0])
+  else:
+    queue.pop(0)
 
-# 入力
-Q = int(input()) # クエリ数
-queries = [ input().split() for i in range(Q) ] # クエリの情報（各要素は ["1", 名前を表す文字列] or ["2"] or ["3"]）
 
-# クエリの処理
-T = deque()
-for q in queries:
-	if q[0] == "1":
-		T.append(q[1])
-	if q[0] == "2":
-		print(T[0])
-	if q[0] == "3":
-		T.popleft()
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_az
+"""
