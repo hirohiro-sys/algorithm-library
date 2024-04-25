3c40bbfa305f6a65fff467851e4325320e04ee4d
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-25 10:16:28 +0900

    基本的なDP

--- Chapter4/A16.py
+++ Chapter4/A16.py
@@ -1,15 +1,14 @@
-# 入力
 N = int(input())
 A = list(map(int,input().split()))
 B = list(map(int,input().split()))
 
-# 動的計画法
-dp = [ None ] * (N+1)
+dp = [0] * 100001
 dp[1] = 0
 dp[2] = A[0]
 for i in range(3,N+1):
-  # この式については解説みる
   dp[i] = min(dp[i-1]+A[i-2], dp[i-2]+B[i-3])
-  
-# 出力
 print(dp[N])
+
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_p
+"""
