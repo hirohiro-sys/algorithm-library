c05c66a365c68fc44269a8b3c0bfea4305d3dabe
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-25 10:17:11 +0900

    基本的なDP(2)

--- Chapter4/B16.py
+++ Chapter4/B16.py
@@ -1,14 +1,15 @@
-# 入力
 N = int(input())
 H = list(map(int, input().split()))
 
-dp = [ None ] * N
-
-# 動的計画法
+dp = [0] * N
 dp[0] = 0
 dp[1] = abs(H[0] - H[1])
 for i in range(2, N):
+	# 漸化式は問題文通り
 	dp[i] = min(dp[i-1] + abs(H[i-1]-H[i]), dp[i-2] + abs(H[i-2]-H[i]))
 
-# 出力
-print(dp[N-1])
+print(dp[-1])
+
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/dp_a
+"""
