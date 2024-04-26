3817bc4f1bd1c9a2bfed8024faf639d905504555
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-26 21:52:47 +0900

    基本的な優先度付きキュー

--- Chapter8/A53.py
+++ Chapter8/A53.py
@@ -1,18 +1,16 @@
 import heapq
 
-# 入力
-Q = int(input()) # クエリ数
-queries = [ input().split() for i in range(Q) ] # クエリの情報（各要素は ["1", 値段を表す文字列] or ["2"] or ["3"]）
+prio_queue = []
+for _ in range(int(input())):
+    query = input().split()
+    if query[0]=="1":
+        heapq.heappush(prio_queue,int(query[1]))
+    elif query[0]=="2":
+        print(prio_queue[0])
+    else:
+        heapq.heappop(prio_queue)
 
-# クエリの処理
-T = []
-for q in queries:
-	if q[0] == "1":
-		heapq.heappush(T, int(q[1]))
-	if q[0] == "2":
-		print(T[0]) # T[0] は「優先度付きキューの中の最小要素」になっている
-	if q[0] == "3":
-		heapq.heappop(T)
 
-# 注意 1：Python の heapq モジュールは、deque モジュールとは異なり、関数を使って list を操作するという形式になっています。
-# 注意 2：優先度付きキューの最小要素は T[0] で取り出せますが、例えば小さいほうから 2 番目の要素が T[1] で取り出せるとは限らないことに注意してください。
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ba
+"""
