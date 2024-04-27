6ab5c7f7e853b7e7f0f0eb02bc4980aefb2e891a
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-27 17:53:23 +0900

    基本的なDP(2)

--- Chapter4/B16.py
+++ Chapter4/B16.py
@@ -2,13 +2,14 @@ N = int(input())
 H = list(map(int, input().split()))
 
 dp = [0] * N
+# インデックスがごちゃごちゃになるのを防ぐため0から埋める
 dp[0] = 0
 dp[1] = abs(H[0] - H[1])
 for i in range(2, N):
-	# 漸化式は問題文通り
+	# i+1する場合とi+2する場合でどちらが小さいか
 	dp[i] = min(dp[i-1] + abs(H[i-1]-H[i]), dp[i-2] + abs(H[i-2]-H[i]))
 
-print(dp[-1])
+print(dp[-1]) #dp[n-1]
 
 """
 https://atcoder.jp/contests/tessoku-book/tasks/dp_a
