a58b7672f69377530991bad42b887e425c1f1652
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-29 20:32:45 +0900

    応用的な深さ優先探索

--- Chapter9/B62.py
+++ Chapter9/B62.py
@@ -1,37 +1,30 @@
 import sys
-
 sys.setrecursionlimit(1 << 20)
 
-# 入力
-n, m = map(int,input().split())
-
-# 隣接リスト初期化&構築
-g  = [[] for _ in range(n)]
-for i in range(m):
+n,m = map(int,input().split())
+g = [[] for _ in range(n)]
+for _ in range(m):
     a,b = map(int,input().split())
-    g[a-1].append(b)
-    g[b-1].append(a)
+    g[a-1].append(b-1)
+    g[b-1].append(a-1)
 
-# ノードの訪問状態管理配列を初期化
+# ここの処理の流れがわかりづらかったら公式のGithub見るといい
 visited = [False] * n
-
-# パスを記録するリスト
 path = []
-
-# 深さ優先探索メイン実装
-def dfs(i: int):
-    path.append(i)
-
-    if i==n - 1:
-        for x in path:
-            print(x + 1)
-        exit(0)
-
-    visited[i] = True
-    for j in g[i]:
+def dfs(v):
+    path.append(v)
+    if v==n-1:
+        for i in path:
+            print(i+1)
+        exit()
+    visited[v]=True
+    for j in g[v]:
         if not visited[j]:
             dfs(j)
     path.pop()
 
-# 深さ優先探索
 dfs(0)
+
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ei
+"""
