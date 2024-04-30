f130f01acbb5bdc2f83300f3c33044975e8be678
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-30 09:17:15 +0900

    基本的な幅優先探索

--- Chapter9/A63.py
+++ Chapter9/A63.py
@@ -1,38 +1,28 @@
 from collections import deque
 
-# 入力
 n,m = map(int,input().split())
+G = [[] for _ in range(n+1)]
+for _ in range(m):
+  a,b = map(int,input().split())
+  G[a].append(b)
+  G[b].append(a)
 
-edges = [list(map(int,input().split())) for _ in range(m)]
 
-# 隣接リストの初期化と構築
-G = [[] for i in range(n+1)]
-for a,b in edges:
-    G[a].append(b)
-    G[b].append(a)
-
-# 各頂点までの距離を保持するリストdistを初期化
-dist = [-1] * (n + 1)
-
-# 始点からの距離を初期化
+queue = deque()
+queue.append(1)
+dist = [-1] * (n+1)
 dist[1] = 0
+while queue:
+  pos = queue.popleft()
+  for i in G[pos]:
+    if dist[i]==-1:
+      dist[i] = dist[pos] + 1
+      queue.append(i)
 
-# 幅優先探索のキューを初期化
-Q = deque()
-Q.append(1)
-
-# 幅優先探索メイン処理
-while len(Q) >= 1:
-    pos = Q.popleft()
-    for nex in G[pos]:
-        if dist[nex] == -1:
-            dist[nex] = dist[pos] + 1
-            Q.append(nex)
-
-for i in range(1,n + 1):
-    print(dist[i])
+for i in range(1,n+1):
+  print(dist[i])
 
 
-'''
-書籍の図を見ながらコードを見ていくとわかりやすい
-'''
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_an
+"""
