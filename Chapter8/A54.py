3310fa90606a8e2bf8b043f47ad8ede8c8a5f83f
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-05-01 14:48:48 +0900

    基本的な連想配列

--- Chapter8/A54.py
+++ Chapter8/A54.py
@@ -1,11 +1,11 @@
-# 入力
-Q = int(input()) # クエリ数
-queries = [ input().split() for i in range(Q) ] # クエリの情報（各要素は ["1", 名前を表す文字列, 得点を表す文字列] or ["2", 名前を表す文字列]）
-
-# クエリの処理
 Map = {}
-for q in queries:
-	if q[0] == "1":
-		Map[q[1]] = q[2]
-	if q[0] == "2":
-		print(Map[q[1]])
+for _ in range(int(input())):
+  Query = input().split()
+  if Query[0]=="1":
+    Map[Query[1]]=Query[2]
+  else:
+    print(Map[Query[1]])
+
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bb
+"""
