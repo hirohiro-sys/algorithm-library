45677f67b55074d77c48efad29a2959847d54dc5
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-05-03 10:20:23 +0900

    基本的なしゃくとり法(2)

--- Chapter3/B13.py
+++ Chapter3/B13.py
@@ -1,29 +1,13 @@
-# A[l]からA[r]までの合計値
-def sum(l,r,S):
-  return S[r+1] - S[l]
-
-# 入力
 N,K = map(int,input().split())
 A = list(map(int,input().split()))
+ans = 0
+right = 0
+for left in range(N):
+  while right<N and sum(A[left:right+1])<=K:
+    right += 1
+  ans += right-left
+print(ans)
 
-# 累積和
-S = [ 0 ] * (N + 1)
-for i in range(1,N+1):
-  S[i] = S[i-1] + A[i-1]
-  
-R = [ None ] * N
-
-# しゃくとり法
-for i in range(N):
-  if i==0:
-    R[i] = -1
-  else:
-    R[i] = R[i-1]
-  while R[i] < N-1 and sum(i,R[i]+1,S) <= K:
-    R[i] += 1
-
-# 出力
-Answer = 0
-for i in range(N):
-  Answer += (R[i] - i + 1)
-print(Answer)
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cl
+"""
