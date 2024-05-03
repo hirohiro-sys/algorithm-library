4190c1868111c651cee4670116917f19e6d7c7b2
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-05-03 10:19:41 +0900

    基本的なしゃくとり法

--- Chapter3/A13.py
+++ Chapter3/A13.py
@@ -1,22 +1,15 @@
-# 入力
 N,K = map(int,input().split())
 A = list(map(int,input().split()))
 
-R = [ None ] * N
+right = 0
+ans = 0
+for left in range(N):
+  while right+1<N and A[right+1]-A[left]<=K:
+    right += 1
+  ans += right-left
 
-# しゃくとり法
-for i in range(N-1):
-  # スタート地点
-  if i==0:
-    R[i] = 0
-  else:
-    R[i] = R[i - 1]
-  # 限界まで増やす(この部分は図で見るとわかりやすい)
-  while R[i] < N-1 and A[R[i]+1]-A[i] <= K:
-    R[i] += 1
+print(ans)
 
-# 出力
-Answer = 0
-for i in range(N-1):
-  Answer += (R[i] - i)
-print(Answer)
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_m
+"""
