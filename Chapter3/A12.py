4146ecf243e1614dc86798767936e384ae8cbf81
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-05-05 16:36:38 +0900

    答えで二分探索

--- Chapter3/A12.py
+++ Chapter3/A12.py
@@ -1,24 +1,20 @@
-# 答えがx以下かどうかを判定する関数
-def check(x,N,K,A):
-  sum = 0
-  for i in range(N):
-    sum += x // A[i]
-  if sum >= K:
-      return True
-  return False
+n,k = map(int,input().split())
+a = list(map(int,input().split()))
 
-# 入力&二分探索
-N,K = map(int,input().split())
-A = list(map(int,input().split()))
-Left = 1
-Right = 10 ** 9
-while Left < Right:
-  Mid = (Left + Right) // 2
-  Answer = check(Mid,N,K,A)
+left=0
+right=10**9
+while left < right:
+  sum = 0
+  middle = (left+right)//2
+  for i in range(n):
+    sum += middle//a[i]
+  if k<=sum:
+    right = middle
+  else:
+    left = middle + 1
   
-  if Answer == False:
-    Left = Mid + 1
-  if Answer == True:
-    Right = Mid
-# 出力    
-print(Left)
+print(left)
+
+"""
+https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_l
+"""
