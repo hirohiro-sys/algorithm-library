e306b432166dc3cc7e518db15901bb2cfc206396
hirokazu kato <126783940+hirohiro-sys@users.noreply.github.com>
2025-08-25 18:59:25 +0900

    A06.pyのコード理解

--- Chapter2/A06.py
+++ Chapter2/A06.py
@@ -1,12 +1,18 @@
-n,q = map(int,input().split())
-a = list(map(int,input().split()))
-s = [0] * (n+1)
-s[0] = 0
-for i in range(1,n+1):
-  s[i] = s[i-1]+a[i-1]
-for i in range(q):
-  a,b = map(int,input().split())
-  print(s[b]-s[a-1])
-"""
+N,Q = map(int,input().split())
+A = list(map(int,input().split()))
+
+# prefixSum[L]に1が来ることもあるのでS[0]を用意
+prefixSum = [0] * (N+1)
+
+# 累積和を計算
+for i in range(1,N+1):
+  prefixSum[i] = prefixSum[i-1] + A[i-1]
+
+# 区間の総和を出力
+for _ in range(Q):
+  L,R = map(int,input().split())
+  print(prefixSum[R]-prefixSum[L-1])
+  
+"""問題URL
 https://atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_ai
 """
