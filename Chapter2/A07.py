574ecfa15ca6e7e3f3e3fc50d2989a0e660f985b
hirokazu kato <126783940+hirohiro-sys@users.noreply.github.com>
2025-08-26 18:45:34 +0900

    A07.py コード理解

--- Chapter2/A07.py
+++ Chapter2/A07.py
@@ -1,15 +1,20 @@
 N = int(input())
 D = int(input())
-A = [0] * (N+2)
+
+# 前日比の計算
+A = [0] * (N+1)
 for i in range(D):
-  L,R = map(int,input().split())
-  A[L] += 1
-  A[R+1] -= 1
+    L, R = map(int, input().split())
+    A[L] += 1
+    if R + 1 <= N:
+        A[R+1] -= 1
+
+# 累積和を計算しつつ出力
 ans = 0
 for i in range(N):
-  ans += A[i+1]
-  print(ans)
+    ans += A[i+1]
+    print(ans)
   
-"""
+"""問題URL
 https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_g
 """
