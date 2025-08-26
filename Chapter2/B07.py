4b854103cde6add56aa41937b9a3c0933268fed0
hirokazu kato <126783940+hirohiro-sys@users.noreply.github.com>
2025-08-26 18:54:04 +0900

    B07.py コード理解

--- Chapter2/B07.py
+++ Chapter2/B07.py
@@ -1,15 +1,19 @@
 T = int(input())
 N = int(input())
+
+# 前日比を求める
 A = [0] * (T+1)
-for i in range(N):
+for _ in range(N):
   L,R = map(int,input().split())
   A[L] += 1
   A[R] -= 1
+  
+# 累積和を求める
 ans = 0
 for i in range(T):
   ans += A[i]
   print(ans)
 
-"""
+"""問題URL
 https://atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_al
 """
