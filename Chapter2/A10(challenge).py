da5a8aeab025e1d5fbdd60a06b1698208e55ccbf
hirokazu kato <126783940+hirohiro-sys@users.noreply.github.com>
2025-08-30 22:26:54 +0900

    A10(challenge).py コード理解

--- Chapter2/A10(challenge).py
+++ Chapter2/A10(challenge).py
@@ -1,28 +1,28 @@
-# 入力
 N = int(input())
 A = list(map(int,input().split()))
 D = int(input())
-query = [list(map(int,input().split())) for _ in range(D)]
-for q in query:
+Q = [list(map(int,input().split())) for _ in range(D)]
+
+# 0idx調整
+for q in Q:
   q[0] -= 1
   q[1] -= 1
 
 # 左からの最大累積和
-left_max = [0] * N
-left_max[0] = A[0]
+left_mx = [0] * N
+left_mx[0] = A[0]
 for i in range(1,N):
-  left_max[i] = max(left_max[i-1],A[i])
-  
+  left_mx[i] = max(left_mx[i-1],A[i])
+
 # 右からの最大累積和
-right_max = [0] * N
-right_max[-1] = A[-1]
+right_mx = [0] * N
+right_mx[-1] = A[-1]
 for i in range(N-2,-1,-1):
-  right_max[i] = max(right_max[i+1],A[i])
+  right_mx[i] = max(right_mx[i+1],A[i])
+  
+for l,r in Q:
+  print(max(left_mx[l-1],right_mx[r+1]))
 
-# 出力
-for l,r in query:
-  print(max(left_max[l-1],right_max[r+1]))
-
-"""
+"""問題URL
 https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_j
 """
